ed506f4a69564c8327627dafd13c5bdb2226482e
张少龙 <zhangshaolong@baijiahulian.com>
2015-07-31 16:09:23 +0800

    添加解析模板文件并预编译模板功能

---
+++ simplite/demo/templates.tpl
@@ -0,0 +1,26 @@
+{{ -- tpl:table-tpl -- }}
+<table>
+    <head>
+        <tr>
+            <th>姓名</th>
+            <th>性别</th>
+            <th>年龄</th>
+        </tr>
+    </head>
+    <body>
+        <tr>
+            <td><%=_this.name%></td>
+            <td><%=_this.gender%></td>
+            <td><%=_this.age%><% include('juhuixue-index-tpl', _this.c); %></td>
+        </tr>
+    </body>
+</table>
+{{ -- /tpl -- }}
+
+{{ -- tpl:juhuixue-index-tpl -- }}
+<div>
+    <div><%=_this.name%></div>
+    <div><%=#_this.gender%></div>
+    <div><%=_this.age%></div>
+</div>
+{{ -- /tpl -- }}
---
+++ simplite/src/template-loader.js
@@ -0,0 +1,26 @@
+/**
+ * @file Simplate模板加载器，自动进行预编译
+ * @author：张少龙（zhangshaolongjj@163.com）
+ */
+define(function (require) {
+    var Simplite = require('simplite');
+    var tplReg = /\{\{\s*\-\-\s*tpl\s*\:\s*([^\}\s]+)\s*\-\-\s*\}\}\s*([\s\S]+?)\{\{\s*\-\-\s*\/tpl\s*\-\-\s*\}\}/g;
+    return function (path, simplite, callback) {
+        if (!(simplite instanceof Simplite)) {
+            callback = simplite;
+            simplite = Simplite;
+        }
+
+        requirejs(['text!' + path], function (text) {
+            var templateMap = {};
+            text.replace(tplReg, function (all, tplId, tplContent) {
+                simplite.addTemplate(tplId, tplContent);
+                simplite.compile(tplId, simplite);
+                templateMap[tplId] = tplContent;
+            });
+            if (callback) {
+                callback(simplite, templateMap);
+            }
+        });
+    };
+});
